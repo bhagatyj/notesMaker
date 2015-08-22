16bddd5deab76390c2f2534069dcda965cde3584
Bhagat Janarthanan <bhagatyj@gmail.com>
2015-08-21 22:10:23 -0700

    Introducing nodes

--- app.py
+++ app.py
@@ -82,56 +82,120 @@ def findMdFilesInternal( dirname ):
     mySet["mdFiles"] = httpLinks
     return mySet
 
-def formRecursiveDict( names, separator='___' ):
-    dictRoot = dict()
-    for name in names:
-        newDict = dictRoot
-        nodes = name.split( separator )
-        for node in nodes[ : -1 ]:
-            if node not in newDict:
-                newDict[node] = dict()
-            newDict = newDict[node]
-        if '.files' not in newDict:
-            newDict['.files'] = list()
-        newDict['.files'].append(name)
-    return dictRoot
+class Node:
+    def __init__(self, path, parentPath=""):
+        self.dirs = dict()
+        self.files = list()
+        self.name = path
+        self.fullName = parentPath + "/" + path
 
+    def __str__(self):
+        return "Node: " + self.fullName
+
+    def addSubNode(self, name):
+        if name not in self.dirs:
+            self.dirs[name] = Node(name, self.fullName)
+        return self.dirs[name]
+
+    def getSubNode(self, name):
+        if name in self.dirs:
+            return self.dirs[name]
+        else:
+            return None
+
+
+def formRecursiveDict( names, separator='___' ):
+    dictRoot = Node("root")
+    for pathName in names:
+        pathList = pathName.split( '___' )
+        # Run once through the list and create all the dirs.
+        # Use only upto path[:-1]
+        subNode = dictRoot
+        for nodeName in pathList[:-1]:
+            subNode = subNode.addSubNode( nodeName )
+        # Run once more through the list and create all the files.
+        subNode = dictRoot
+        for nodeName in pathList:
+            if nodeName not in subNode.dirs:
+                print "added file:", nodeName
+                #subNode.files.append(nodeName)
+                subNode.files.append(pathName)
+            else:
+                subNode = subNode.dirs[nodeName]
+    return dictRoot
+    
 def pretty_items(htmlText, inpData, nametag="<strong>%s: </strong>", 
-             itemtag="<li  id='%s.yj.md.html' onclick='changeContent(this)' >%s</li>",
+             itemtag="<li  id='%s.yj.md.html' onclick='changeContent(this)'>%s</li>",
+             itemtagCollapse="<li  id='%s.yj.md.html' onclick='changeContent(this)' class='collapse'>%s</li>",
              valuetag="  %s", blocktag=('<ul>', '</ul>')):
-    print inpData
-    if isinstance(inpData, dict):
-        if '.files' not in inpData:
-            htmlText.append(blocktag[0])
-        for k, v in inpData.iteritems():
+    if isinstance(inpData, list):
+        print inpData 
+        for i in inpData:
+            link = i.split('___')[-1]
+            #htmlText.append(itemtagCollapse % ( i, link ) )
+            htmlText.append(itemtag % ( i, link ) )
+    elif len(inpData.files) > 0:
+        htmlText.append(blocktag[0])
+        #pretty_items(htmlText, inpData.files)
+        for i in inpData.files:
+            link = i.split('___')[-1]
+            #htmlText.append(itemtagCollapse % ( i, link ) )
+            htmlText.append(itemtag % ( i, link ) )
+        htmlText.append(blocktag[1])
+    elif len(inpData.dirs) > 0:
+        htmlText.append(blocktag[0])
+        for k, v in inpData.dirs.iteritems():
             name = nametag % k
-            if isinstance(v, dict) or isinstance(v, list):
-                if (k != '.files'):
-                    htmlText.append(itemtag % ( name, name) )
+            htmlText.append(itemtag % ( name, name) )
+            pretty_items(htmlText, v)
+        htmlText.append(blocktag[1])
+    return htmlText
+
+def pretty_items_old(htmlText, inpData, nametag="<strong>%s: </strong>", 
+             itemtag="<li  id='%s.yj.md.html' onclick='changeContent(this)'>%s</li>",
+             itemtagCollapse="<li  id='%s.yj.md.html' onclick='changeContent(this)' class='collapse'>%s</li>",
+             valuetag="  %s", blocktag=('<ul>', '</ul>')):
+    if isinstance(inpData, dict):
+        k = '.files'
+        if '.files' in inpData:
+            htmlText.append(blocktag[0])
+            v = inpData[k]
+            pretty_items(htmlText, v)
+            htmlText.append(blocktag[1])
+
+        htmlText.append(blocktag[0])
+        for k, v in inpData['dirs'].iteritems():
+            if (k == '.files'):
+                continue
+            name = nametag % k
+            if isinstance(v, dict):
+                htmlText.append(itemtag % ( name, name) )
                 pretty_items(htmlText, v)
             else:
-                value = valuetag % v
-                htmlText.append(itemtag % (name + value, name + value ))
-        if '.files' not in inpData:
-            htmlText.append(blocktag[1])
+                assert(0)
+        htmlText.append(blocktag[1])
     elif isinstance(inpData, list):
-        htmlText.append(blocktag[0])
         for i in inpData:
             if isinstance(i, dict) or isinstance(i, list):
                 htmlText.append(itemtag % (" - ", " - " ) )
                 pretty_items(htmlText, i)
             else:
                 link = i.split('___')[-1]
+                #htmlText.append(itemtagCollapse % ( i, link ) )
                 htmlText.append(itemtag % ( i, link ) )
-        htmlText.append(blocktag[1])
     return htmlText
 
+# pretty_items is a tricky function. We need the list we pass to the function. But
+# the pretty_items function uses recursion and hence cannot return it directly.
+# So we hold a reference.
 def formHtmlText( inpData ):
-    r = list()
-    pretty_items( r, inpData )
-    return '\n'.join(r)
+    reference = list()
+    pretty_items( reference, inpData )
+    print reference
+    return '\n'.join(reference)
 
 def createHtmlDivOfFiles( files ):
+    print files
     rDict = formRecursiveDict( files )
     htmlDiv = formHtmlText( rDict )
     return htmlDiv
@@ -139,7 +203,6 @@ def createHtmlDivOfFiles( files ):
 def createHomePage():
    files = findMdFilesInternal( '' )[ 'mdFiles' ]
    files.sort()
-   print files
    htmlFile = HOST_DIR + 'pages/' + '/index.html'
    with open(APP_ROOT + "home_page_header.html") as f: 
        HOME_PAGE_HEADER = "".join( f.readlines() )
